ab77043fb9b88102e95c12b98aac2f4748a1d654
vanphunhan <johnyvan1993@gmail.com>
2019-12-01 23:48:18 +0700

    - a

--- app/src/main/java/com/johy/countdowntotet/MainActivity.kt
+++ app/src/main/java/com/johy/countdowntotet/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.johy.countdowntotet
 
-import android.content.Intent
 import android.content.res.Configuration
+import android.media.MediaPlayer
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.widget.LinearLayout
 import com.johy.countdowntotet.base.BaseActivity
 import com.johy.countdowntotet.databinding.ActivityMainBinding
 import com.johy.countdowntotet.defines.Constants.ONE_SECOND
@@ -20,24 +18,36 @@ open class MainActivity :
             return resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT
         }
 
+    private var mPlayer: MediaPlayer? = null
+
     override fun onUIInitialized() {
+        mPlayer = MediaPlayer.create(this, R.raw.tet_nguyen_dan)
         mViewModel?.parse()
     }
 
     override fun onWorksInitialized() {
     }
 
-    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
-        super.onRestoreInstanceState(savedInstanceState)
-    }
-
     override fun onStart() {
         super.onStart()
+        intent.extras?.getInt("paused_time")?.let { mPlayer?.seekTo(it) }
+        if (mPlayer?.isPlaying != true) mPlayer?.start()
         mViewModel?.mRunnable?.let { mViewModel?.mHandler?.postDelayed(it, ONE_SECOND) }
     }
 
     override fun onPause() {
         mViewModel?.mRunnable?.let { mViewModel?.mHandler?.removeCallbacks(it) }
+        if (mPlayer?.isPlaying == true) mPlayer?.pause()
+        val bundle = intent.extras ?: Bundle()
+        bundle.putInt("paused_time", mPlayer?.currentPosition ?: 0)
+        intent.putExtras(bundle)
         super.onPause()
+    }
+
+    override fun onDestroy() {
+        if (mPlayer?.isPlaying == true) mPlayer?.stop()
+        mPlayer?.release()
+        mPlayer = null
+        super.onDestroy()
     }
 }
